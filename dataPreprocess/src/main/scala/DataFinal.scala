edafa9e5726006d26cc239887b2181ed5cb6580f
Jingru Xiang <xiang.jing@northeastern.edu>
2022-04-30 23:59:16 -0400

    define method

--- dataPreprocess/src/main/scala/DataFinal.scala
+++ dataPreprocess/src/main/scala/DataFinal.scala
@@ -1,5 +1,6 @@
-import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.{DataFrame, SparkSession}
 import org.apache.spark.sql.functions._
+import org.apache.spark.sql.types.{DoubleType, IntegerType}
 
 object DataFinal{
   def main(args: Array[String]) {
@@ -23,18 +24,12 @@ object DataFinal{
      *  Create new dataframe userData(userId, gameName, purchase)
      */
     val dataFrame = spark.read.option("delimiter", ",").option("header", "true").csv("./src/main/resources/steam-200k.csv")
-    val schemas = Seq("userId", "gameName", "purchase")
-    val userData = dataFrame.select(dataFrame("user_id"), dataFrame("name"), dataFrame("time"))
-      .where("behavior_name = 'purchase'").toDF(schemas: _*)
+    val userData = getUser(dataFrame)
 
     /**
-     *  Read steam.csv
-     *  Get columns and process data from steam.csv
-     *  Create new dataframe gameData(gameName, gameTags, gameRating, ID)
-     */
-    import org.apache.spark.sql.types.{IntegerType, DoubleType}
-
-    /**
+     * Read steam.csv
+     * Get columns and process data from steam.csv
+     * Create new dataframe gameData
      * Game Schema:
      * - gameName
      * - gameTags
@@ -43,26 +38,15 @@ object DataFinal{
      * - id(gameId)
      */
     val df = spark.read.option("delimiter", ",").option("header", "true").csv("./src/main/resources/steam.csv")
-    val schema = Seq("gameName", "gameTags", "ratingCount", "gameRating")
-    val game = df.join(userData, userData("gameName") === df("name"), "leftsemi")
-    var gameData = game.select(game("name"), game("genres"), game("positive_ratings") + game("negative_ratings"),
-      game("positive_ratings") * 10/(game("positive_ratings") + game("negative_ratings"))).toDF(schema: _*)
-    gameData = gameData.withColumn("id", monotonically_increasing_id).withColumn("id", col("id").cast(IntegerType))
-
-    gameData.show()
+    val gameData = getGameData(df, userData)
 
     /**
-     * TrainSet and TestSet Schema:
+     * User Schema:
      * - id(gameId): Int
-     * - gameName: String
      * - userId: Int
      * - purchase(Preference): 0 / 1 (Double)
      */
-    val user = userData.as("temp1").join(gameData.as("temp2"), userData("gameName") === gameData("gameName"), "inner")
-      .select(col("temp2.id"), col("temp1.gameName"), col("temp1.userId"), col("temp1.purchase"))
-    val userBehavior = user.withColumn("userId", col("userId").cast(IntegerType))
-      .withColumn("purchase", col("purchase").cast(DoubleType))
-    userBehavior.show()
+    val userBehavior = getUserData(userData, gameData)
 
     /**
      * Temp Table:
@@ -73,18 +57,11 @@ object DataFinal{
       .withColumn("id", explode($"__temporarily__"))
       .select(col("id"), col("userId"), col("purchase")*0)
       .distinct()
-    println(userTemp.count())
 
-    var userFin = userTemp.as("t1").
-      join(userBehavior.as("t2"), userBehavior("userId") === userTemp("userId") && userBehavior("id") === userTemp("id"), "left")
-      .select(col("t1.id"), col("t1.userId"), col("t1.(purchase * 0)"), col("t2.purchase"))
-      .distinct()
-      .na.fill(0)
-    val sch = Seq("id", "userId", "purchase")
-    userFin = userFin.select(userFin("id"), userFin("userId"), userFin("(purchase * 0)")+ userFin("purchase")).toDF(sch: _*)
+    val userFin = finalUserData(userTemp, userBehavior)
 
     /**
-     * Separate userData to 70% train data and 30% test data
+     * Separate userData to 70% train data, 20% validation data, and 10% test data
      */
     val splitData = userFin.orderBy(rand()).randomSplit(Array(0.7, 0.2, 0.1))
     val trainSet = splitData(0)
@@ -107,6 +84,41 @@ object DataFinal{
     MongoSpark.save(gameData.write.option("collection", "game").mode("overwrite"))
 
     spark.stop()
+  }
+
+  def getUser(dataFrame: DataFrame): DataFrame ={
+    val schemas = Seq("userId", "gameName", "purchase")
+    val userData = dataFrame.select(dataFrame("user_id"), dataFrame("name"), dataFrame("time"))
+      .where("behavior_name = 'purchase'").toDF(schemas: _*)
+    userData
+  }
+
+  def getGameData(df: DataFrame, userData: DataFrame): DataFrame ={
+    val schema = Seq("gameName", "gameTags", "ratingCount", "gameRating")
+    val game = df.join(userData, userData("gameName") === df("name"), "leftsemi")
+    var gameData = game.select(game("name"), game("genres"), game("positive_ratings") + game("negative_ratings"),
+      game("positive_ratings") * 10/(game("positive_ratings") + game("negative_ratings"))).toDF(schema: _*)
+    gameData = gameData.withColumn("id", monotonically_increasing_id).withColumn("id", col("id").cast(IntegerType))
+    gameData
+  }
+
+  def getUserData(userData: DataFrame, gameData: DataFrame): DataFrame ={
+    val user = userData.as("temp1").join(gameData.as("temp2"), userData("gameName") === gameData("gameName"), "inner")
+      .select(col("temp2.id"), col("temp1.gameName"), col("temp1.userId"), col("temp1.purchase"))
+    val userBehavior = user.withColumn("userId", col("userId").cast(IntegerType))
+      .withColumn("purchase", col("purchase").cast(DoubleType))
+    userBehavior
+  }
+
+  def finalUserData(userTemp: DataFrame, userBehavior: DataFrame): DataFrame ={
+    var userFin = userTemp.as("t1").
+      join(userBehavior.as("t2"), userBehavior("userId") === userTemp("userId") && userBehavior("id") === userTemp("id"), "left")
+      .select(col("t1.id"), col("t1.userId"), col("t1.(purchase * 0)"), col("t2.purchase"))
+      .distinct()
+      .na.fill(0)
+    val sch = Seq("id", "userId", "purchase")
+    userFin = userFin.select(userFin("id"), userFin("userId"), userFin("(purchase * 0)")+ userFin("purchase")).toDF(sch: _*)
+    userFin
   }
 
 }
